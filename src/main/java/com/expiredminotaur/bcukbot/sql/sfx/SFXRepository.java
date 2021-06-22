cf6b28d025405c53ed479b8a96dd4a41743a18d1
Daniel M <danielm59@gmail.com>
2021-06-22 18:01:27 +0100

    SFX can now have a category assigned to them

--- src/main/java/com/expiredminotaur/bcukbot/sql/sfx/SFXRepository.java
+++ src/main/java/com/expiredminotaur/bcukbot/sql/sfx/SFXRepository.java
@@ -16,23 +16,29 @@ public interface SFXRepository extends CrudRepository<SFX, Integer>
     List<SFX> findAll();
 
     @Query("from SFX where hidden=false")
-    @NotNull
-    List<SFX> getSFXList();
+    @Cacheable(value = "SFX_NOT_HIDDEN")
+    @NotNull List<SFX> getSFXList();
 
     @Query("from SFX where lower(triggerCommand)=:trigger")
     @Cacheable(value = "SFX")
+    @NotNull List<SFX> findByTrigger(String trigger);
+
+    @Cacheable(value = "SFX")
+    @NotNull List<SFX> findByCategory(SFXCategory category);
+
+    @Cacheable(value = "SFX_NULL")
     @NotNull
-    List<SFX> findByTrigger(String trigger);
+    List<SFX> findByCategoryIsNull();
 
     @Override
-    @CacheEvict(value = "SFX", allEntries = true)
+    @CacheEvict(value = {"SFX", "SFX_NULL", "SFX_NOT_HIDDEN"}, allEntries = true)
     void deleteById(@NotNull Integer id);
 
     @Override
-    @CacheEvict(value = "SFX", allEntries = true)
+    @CacheEvict(value = {"SFX", "SFX_NULL", "SFX_NOT_HIDDEN"}, allEntries = true)
     void delete(@NotNull SFX sfx);
 
     @Override
-    @CacheEvict(value = "SFX", allEntries = true)
+    @CacheEvict(value = {"SFX", "SFX_NULL", "SFX_NOT_HIDDEN"}, allEntries = true)
     @NotNull <S extends SFX> S save(@NotNull S sfx);
 }
